fe4c6ae0eedc6be4aa629fc1ab86ad4204c79f35
Jide Tireni <jtirenipraise@gmail.com>
2025-08-11 23:53:47 +0100

    Add helper function to load PDF files

---
+++ data/helper.go
@@ -0,0 +1,22 @@
+package data
+
+import (
+	"io"
+	"os"
+)
+
+func loadPDFFile(path string) (io.ReaderAt, int64, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return file, info.Size(), nil
+}
